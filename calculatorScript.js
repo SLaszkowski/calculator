3162e6c02d10b5bbd9502c8f7f35539ec145aadb
SLaszkowski <sebastian.laszkowski1@gmail.com>
2025-01-09 16:57:14 +0100

    Add buttons to js

---
+++ calculatorScript.js
@@ -0,0 +1,4 @@
+const btn = document.querySelectorAll('.btn');
+const btnOperator = document.querySelectorAll(".btnOperator");
+const btnClear = document.querySelector(".btnClear");
+const btnEqual = document.querySelector(".btnEqual");
