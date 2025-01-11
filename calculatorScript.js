d1226f8320d2c65aede2b08736c902c7ab18a3cf
SLaszkowski <sebastian.laszkowski1@gmail.com>
2025-01-11 12:39:53 +0100

    Add event listener for buttons

--- calculatorScript.js
+++ calculatorScript.js
@@ -1,4 +1,27 @@
 const btn = document.querySelectorAll('.btn');
-const btnOperator = document.querySelectorAll(".btnOperator");
-const btnClear = document.querySelector(".btnClear");
-const btnEqual = document.querySelector(".btnEqual");
+const display = document.querySelector(".display");
+
+function updateDisplay(btnValue, btnType) {
+    switch(btnType) {
+        case "number":
+            display.innerText += btnValue;
+            break;
+        case "operator":
+            display.innerText += btnValue;
+            break;
+        case "action":
+            if(btnValue === "C") display.innerText = "0";
+            break;
+        case "dot":
+            display.innerText += btnValue;
+            break;
+    }
+}
+
+btn.forEach(button => {
+    button.addEventListener('click', (event) => {
+        const btnValue = event.target.getAttribute("data-value");
+        const btnType = event.target.getAttribute("data-type");
+        updateDisplay(btnValue, btnType);
+    })
+});
