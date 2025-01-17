67e1db91975c01d59135d95b4184861f2075fad6
SLaszkowski <sebastian.laszkowski1@gmail.com>
2025-01-17 12:47:09 +0100

    Add count feature

--- calculatorScript.js
+++ calculatorScript.js
@@ -2,31 +2,63 @@ const btn = document.querySelectorAll('.btn');
 const display = document.querySelector(".display");
 const operators = ["+", "-", "x", "/"];
 
+const regOperators = /([+\-*/x])/g;
+const regNumbers = /\d+$/;
+const regSpaces = /\s+/g;
+
 function updateDisplay(btnValue, btnType) {
-    let inputText = display.innerText;
-    let lastChar = inputText.charAt(inputText.length - 1);
+    const inputText = {
+        value: display.innerText,
+        lastChar: display.innerText.charAt(display.innerText.length - 1),
+        removeSpaces: function() { return this.value.replace(regSpaces, ""); },
+        notZero: function() { return this.value != "0"; },
+        includesNumber: function() { return /\d+$/.test(this.value) },
+        includesOperator: function() { return /[+\-*/]/.test(this.value) },
+        replaceOperator: function() { return this.value.replace(/.$/, btnValue) },
+        addSpacesBetweenOperator: function() { return this.value.replace(regOperators, " $1 ") }
+    };
+    inputText.removeSpaces();
 
     switch(btnType) {
         case "number":
-            inputText === "0" ? inputText = btnValue : inputText += btnValue;
+            inputText.notZero()
+                ? inputText.value += btnValue
+                : inputText.value = btnValue
             break;
         case "operator":
-            if (inputText != "0") {
-                operators.includes(lastChar) ? inputText = inputText.replace(/.$/, btnValue)  : inputText += btnValue;
+            if (inputText.notZero()) {
+                operators.includes(inputText.lastChar)
+                    ? inputText.value = inputText.replaceOperator()
+                    : inputText.value += btnValue;
             }
             break;
         case "action":
-            if(inputText != "0") {
-                if(btnValue === "C") inputText = "0";
+            if(inputText.notZero()) {
+                if(btnValue === "C") inputText.value = "0"
+                else if(inputText.includesNumber() && inputText.includesOperator()) inputText.value = count(inputText.value, btnValue);
             }
             break;
         case "dot":
             break;
     }
-    // Add spaces before and after operators
-    const inputTextForDisplay = inputText => inputText.replace(/\s+/g, "").replace(/([+\-*/x])/g, " $1 ");
 
-    display.innerText = inputTextForDisplay(inputText);
+    inputText.includesOperator() ? display.innerText = inputText.addSpacesBetweenOperator() : display.innerText = inputText.value;
+}
+
+function count(inputText, btnValue) {
+    const operator = inputText.match(/[+\-*/]/);
+    const values = inputText.split("+").map(Number);
+    console.log(values)
+    switch(operator[0]) {
+        case "+":
+            return values[0] + values[1];
+        case "-":
+            return values[0] - values[1];
+        case "/":
+            return values[0] / values[1];
+        case "x":
+            return values[0] * values[1];
+    }
 }
 
 btn.forEach(button => {
