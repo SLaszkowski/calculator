c8453f25410b0564b85f433125945e4498b66da8
SLaszkowski <sebastian.laszkowski1@gmail.com>
2025-02-20 10:59:30 +0100

    Add validation functions

--- src/validator.js
+++ src/validator.js
@@ -1,10 +1,16 @@
-
 export default class Validator {
-    static parseToNumber(string) {
+    static parseStringToNumber(string) {
+        if (typeof string !== "string") {
+            throw new TypeError(`Invalid input. Expected a valid string, but got: ${typeof string}`)
+        }
         const number = parseFloat(string)
         return isNaN(number) ? null : number;
     }
 
-    static parseToString(number) {
+    static numberToString(number) {
+        if (typeof number !== "number" || isNaN(number)) {
+            throw new TypeError(`Invalid input. Expected a valid number, but got: ${typeof number}`);
+        }
+        return number.toString();
     }
 }
