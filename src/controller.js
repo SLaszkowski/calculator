fcf929a97e64ecd5859891d1b39392c860a496bf
SLaszkowski <sebastian.laszkowski1@gmail.com>
2025-02-20 10:58:49 +0100

    Add first controller actions

--- src/controller.js
+++ src/controller.js
@@ -1,13 +1,13 @@
 import Display from "./display.js";
 import Logic from "./logic.js";
-import Validation from "./validation.js";
+import Validator from "./validator.js";
 
 const buttons = document.querySelectorAll('.btn');
 const mainDisplay = document.querySelector(".mainDisplay");
 const secondDisplay = document.querySelector(".secondDisplay");
 
 const display = new Display(mainDisplay, secondDisplay);
-const logic = new Logic()
+const logic = new Logic();
 
 
 buttons.forEach(button => {
@@ -16,10 +16,15 @@ buttons.forEach(button => {
         const btnType = event.target.getAttribute("data-type");
         switch(btnType) {
             case "number":
+                logic.appendValue(btnValue);
                 break;
             case "operator":
+                logic.storeOperator(btnValue);
                 break;
             case "action":
+                Validator.parseStringToNumber(logic.currentValue);
+                Validator.parseStringToNumber(logic.previousValue);
+                const result = logic.calculate();
                 break;
             case "dot":
                 break;
