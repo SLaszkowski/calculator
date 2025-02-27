bbd0e394d2f7868c1c3bfbcf980a47d85aa4c7ab
SLaszkowski <sebastian.laszkowski1@gmail.com>
2025-02-27 18:28:58 +0100

    Handle dot, remove Validator class from controller

--- src/controller.js
+++ src/controller.js
@@ -1,7 +1,7 @@
 import "../icons.js"
 import Display from "./display.js";
 import Logic from "./logic.js";
-import Validator from "./validator.js";
+import { parseStringToNumber, numberToString  }  from "../src/validator";
 
 const buttons = document.querySelectorAll('.btn');
 const mainDisplay = document.querySelector(".mainDisplay");
@@ -22,8 +22,8 @@ buttons.forEach(button => {
             case "operator":
                 // Second operator passed
                 if(logic.currentValue) {
-                    const a = Validator.parseStringToNumber(logic.currentValue);
-                    const b = Validator.parseStringToNumber(logic.previousValue);
+                    const a = parseStringToNumber(logic.currentValue);
+                    const b = parseStringToNumber(logic.previousValue);
                     if(a && b && logic.operator) logic.result = logic.calculate(a, b);
                 }
                 logic.storeOperator(btnValue);
@@ -32,12 +32,13 @@ buttons.forEach(button => {
                 if(btnValue === "clear") logic.reset();
                 else if(btnValue === "delete") logic.delete();
                 else if(btnValue === "equal") {
-                    const a = Validator.parseStringToNumber(logic.currentValue);
-                    const b = Validator.parseStringToNumber(logic.previousValue);
+                    const a = parseStringToNumber(logic.currentValue);
+                    const b = parseStringToNumber(logic.previousValue);
                     if(a && b && logic.operator) logic.result = logic.calculate(a, b);
                 }
                 break;
             case "dot":
+                if(logic.currentValue && !logic.currentValue.includes(".")) logic.appendValue(btnValue);
                 break;
         }
     });
