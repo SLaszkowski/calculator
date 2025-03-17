ec2cf9da2f04f7fa4dffc6884f792ab616f006d9
SLaszkowski <sebastian.laszkowski1@gmail.com>
2025-03-17 11:06:05 +0100

    Add first cypress e2e test

---
+++ cypress/e2e/ui_spec.cy.js
@@ -0,0 +1,17 @@
+describe("Open calculator", () => {
+  before(() => {
+    cy.visit('http://127.0.0.1:5500/index.html')
+  })
+
+  it('should display all elements correctly', () => {
+    cy.get('.calculator').should('be.visible');
+    cy.get('.calculator__displays').should('be.visible');
+    cy.get('.calculator__main-display').should('be.visible');
+    cy.get('.calculator__second-display').should('be.visible');
+    cy.get('.calculator__buttons').should('be.visible');
+    cy.get('.calculator__btn').should('be.visible')
+    cy.get('.calculator__btn').should('have.length', 19)
+    cy.get('.fa-solid').should('be.visible')
+    cy.get('.fa-solid').should('have.length', 7)
+  })
+})
